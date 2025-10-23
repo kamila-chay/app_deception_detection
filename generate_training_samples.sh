d4548fa1fe10aab0534b3556612fed31549be474
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-10-23 20:15:58 +0200

    Script added

---
+++ generate_training_samples.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p ampere
+#SBATCH -t 24:00:00
+#SBATCH -o out_generate_training_samples.txt
+#SBATCH -e errpr_generate_training_samples.txt
+
+source /home/kamila14/timesformer_experiment/bin/activate
+export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
+
+CUDA_VISIBLE_DEVICES=1 python3 -u generate_training_samples.py 
