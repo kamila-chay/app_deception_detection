c2023f97789e03d18d1c3d2880d23b323cbef1a4
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-10-31 22:04:47 +0100

    Shell scripts for eval

---
+++ eval_clues.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p ampere
+#SBATCH -t 2-00:00:00
+#SBATCH -o out_eval_clues.txt
+#SBATCH -e error_eval_clues.txt
+
+source /home/kamila14/timesformer_experiment/bin/activate
+export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
+
+python3 -u check_clues_aligned.py 
