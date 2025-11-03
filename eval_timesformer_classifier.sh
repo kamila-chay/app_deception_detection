fd387466e80de9cb0c91bff31c8f29fd85919335
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-03 14:39:23 +0100

    Added evaluation for timesformer

---
+++ eval_timesformer_classifier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p ampere
+#SBATCH -t 1-00:00:00
+#SBATCH -o out_eval_timesformer_classifier.txt
+#SBATCH -e error_eval_timesformer_classifier.txt
+
+source /home/kamila14/timesformer_experiment/bin/activate
+export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
+
+CUDA_VISIBLE_DEVICES=0 python3 -u eval_timesformer_classifier.py 
