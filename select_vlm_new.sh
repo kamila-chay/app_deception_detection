cb4865bf485590f64ce6916400dd367620fd0b05
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-05 17:17:42 +0100

    New  shell script

--- select_vlm_new.sh
+++ select_vlm_new.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p turing
+#SBATCH -p ampere
 #SBATCH -t 1-00:00:00
 #SBATCH -o out_select_vlm_new.txt
 #SBATCH -e error_select_vlm_new.txt
@@ -7,4 +7,4 @@
 source /home/kamila14/timesformer_experiment/bin/activate
 export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
 
-python3 -u select_vlm_new.py
+CUDA_VISIBLE_DEVICES=3 python3 -u select_vlm_new.py
