4d1a229baf1aada883ccc813c519e291e0fcf47f
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-08 21:12:35 +0100

    Added qualitative analysis for v3 to diagnose better

---
+++ vis/shell_scripts/qualitative_analysis_v3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p ampere
+#SBATCH -t 1-00:00:00
+#SBATCH -o thesis/out/logs/out_qualitative_analysis_v3.txt
+#SBATCH -e thesis/out/logs/error_qualitative_analysis_v3.txt
+
+source /home/kamila14/timesformer_experiment/bin/activate
+export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
+
+CUDA_VISIBLE_DEVICES=3 python3 -u -m thesis.eval.qualitative_analysis_v3
