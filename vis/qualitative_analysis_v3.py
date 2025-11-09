42883a05a4812f12277d904e20d7733b130a1481
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-09 01:40:40 +0100

    Introduced no repetition rules

--- vis/qualitative_analysis_v3.py
+++ vis/qualitative_analysis_v3.py
@@ -90,7 +90,9 @@ for split_id in range(1, 2):
                 generated_ids = model.generate(**inputs, 
                                                max_new_tokens=1000, 
                                                do_sample=True,
-                                               top_k=10)
+                                               top_k=10,
+                                               repetition_penalty=1.2,
+                                               no_repeat_ngram_size=3)
             generated_ids_trimmed = generated_ids[:, inputs["input_ids"].shape[1] :]
             expected_ids = Y["input_ids"]
             expected_ids_trimmed = expected_ids[:, inputs["input_ids"].shape[1] :]
@@ -146,7 +148,9 @@ for split_id in range(1, 2):
                 generated_ids = model.generate(**inputs, 
                                                max_new_tokens=1000,
                                                do_sample=True,
-                                               top_k=10)
+                                               top_k=10, 
+                                               repetition_penalty=1.2,
+                                               no_repeat_ngram_size=3)
             generated_ids_trimmed = generated_ids[:, inputs["input_ids"].shape[1] :]
             expected_ids = Y["input_ids"]
             expected_ids_trimmed = expected_ids[:, inputs["input_ids"].shape[1] :]
