22586df9e75ba1d4e2f459b74e4ffb64723508c6
Kamila Luchay <kamilaluczaj24@gmail.com>
2025-11-18 11:32:25 +0100

    Typo fix

--- eval/shell_scripts/eval_perception_lm_v6_mrt_val_set_tau_5e-4_lower_lr.sh
+++ eval/shell_scripts/eval_perception_lm_v6_mrt_val_set_tau_5e-4_lower_lr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -p ampere
 #SBATCH -t 2-00:00:00
-#SBATCH -o thesis/out/logs/out_eval_perception_lm_v6_mrt_val_set_tau_5e-3_lower_lr.txt
-#SBATCH -e thesis/out/logs/error_eval_perception_lm_v6_mrt_val_set_tau_5e-3_lower_lr.txt
+#SBATCH -o thesis/out/logs/out_eval_perception_lm_v6_mrt_val_set_tau_5e-4_lower_lr.txt
+#SBATCH -e thesis/out/logs/error_eval_perception_lm_v6_mrt_val_set_tau_5e-4_lower_lr.txt
 
 source /home/kamila14/timesformer_experiment/bin/activate
 export CPLUS_INCLUDE_PATH=/home/kamila14/miniconda3/envs/myenv/include/python3.10
 
-CUDA_VISIBLE_DEVICES=3 python3 -u -m thesis.eval.eval_perception_lm_v6_mrt_val_set_tau_5e-3_lower_lr
+CUDA_VISIBLE_DEVICES=3 python3 -u -m thesis.eval.eval_perception_lm_v6_mrt_val_set_tau_5e-4_lower_lr
