e12f6d412043adba0b4e8d75d5a6b2801dbef1b7
Kamila Luchay <kamilaluczaj24@gmail.com>
2026-01-09 12:44:54 +0100

    Retrieving losses from timesformer classifier

--- train/train_timesformer_classifier.py
+++ train/train_timesformer_classifier.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from math import ceil
 from pathlib import Path
+import json
 
 import deepspeed
 import torch
@@ -21,7 +22,7 @@ from thesis.utils.utils import set_seed
 set_seed(42)
 
 BATCH_SIZE = 72
-GRAD_ACCU_STEPS = 4
+GRAD_ACCU_STEPS = 36
 EPOCHS = 20
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
@@ -70,13 +71,19 @@ for split_id in range(1, 4):
         f"thesis/data/train_fold{split_id}.csv", "thesis/data/video", processor
     )
 
+    val_dataset = DolosClassificationDataset(
+        f"thesis/data/val_fold{split_id}.csv", "thesis/data/video", processor
+    )
+
     lora_model = get_peft_model(model, lora_config)
     lora_model.base_model.model.classifier.weight.requires_grad = True
     lora_model.base_model.model.classifier.bias.requires_grad = True
 
-    for name, param in lora_model.named_parameters():
-        if param.requires_grad:
-            print(name)
+    # lora_model.print_trainable_parameters()
+
+    # for name, param in lora_model.named_parameters():
+    #     if param.requires_grad:
+    #         print(name)
 
     total_steps = ceil(len(train_dataset) / BATCH_SIZE) * EPOCHS
     warmup_steps = int(0.1 * total_steps)
@@ -98,18 +105,34 @@ for split_id in range(1, 4):
         sampler=train_sampler,
     )
 
+    val_sampler = DistributedSampler(val_dataset, get_world_size(), get_rank())
+    val_dataloader = DataLoader(
+        val_dataset,
+        batch_size=BATCH_SIZE // get_world_size() // GRAD_ACCU_STEPS,
+        sampler=val_sampler,
+    )
+
     model_engine.train()
+
+    train_losses_for_this_split = []
+    val_losses_for_this_split = []
 
     for epoch in range(EPOCHS):
         print(f"Epoch {epoch + 1}")
         train_sampler.set_epoch(epoch)
+        total_loss = 0.0
         for batch_x, batch_y in train_dataloader:
             batch_x = batch_x.to(model_engine.device).to(torch.bfloat16)
             batch_y = batch_y.to(model_engine.device)
 
             output = model_engine(pixel_values=batch_x, labels=batch_y)
             model_engine.backward(output.loss)
+            total_loss += output.loss * batch_x.shape[0]
             model_engine.step()
+
+        total_loss /= len(train_dataset)
+
+        train_losses_for_this_split.append(total_loss)
 
         save_path_lora = (
             f"thesis/out/{timestamp}/lora_timesformer_split{split_id}_epoch{epoch}"
@@ -119,3 +142,31 @@ for split_id in range(1, 4):
         )
         lora_model.save_pretrained(save_path_lora)
         torch.save(lora_model.base_model.model.classifier.state_dict(), save_path)
+
+        total_loss = 0
+
+        with torch.inference_mode():
+            for batch_x, batch_y in val_dataloader:
+                batch_x = batch_x.to(model_engine.device).to(torch.bfloat16)
+                batch_y = batch_y.to(model_engine.device)
+
+                output = model_engine(pixel_values=batch_x, labels=batch_y)
+                total_loss += output.loss * batch_x.shape[0]
+
+            total_loss /= len(val_dataset)
+
+            val_losses_for_this_split.append(total_loss)
+        
+        print(train_losses_for_this_split)
+        print(val_losses_for_this_split)
+
+    with open(
+        f"thesis/out/{timestamp}/model_split{split_id}_losses.json", "w"
+    ) as f:
+        json.dump(
+            {
+                "train_losses": train_losses_for_this_split, 
+                "val_losses": val_losses_for_this_split,
+            },
+            f,
+        )
